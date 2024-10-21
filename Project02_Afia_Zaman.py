78d21a662279b7aa1a4566553be54d77f691bff4
AFIA ZAMAN <156888281+Afia-Zaman@users.noreply.github.com>
2024-10-20 19:49:52 -0700

    Add files via upload

---
+++ Project02_Afia_Zaman.py
@@ -0,0 +1,28 @@
+print('|__________|')
+print('|          |')
+print('|___bed____|')
+print('|          |')
+
+print('\033[1m' + '"DEAR LITTLE BABIES, DO NOT BE LIKE THE MONKEYS "' + '\033[0m')
+print()
+print('\033[96m' + '"Five little monkeys"' + '\033[0m')
+Adjective = 'little '
+Animal = 'monkeys '
+Verb1 = 'jumping '
+Noun1 = 'bed '
+Verb2 = 'bumped '
+Noun2 = 'Doctor '
+print('Five '+ Adjective + Animal + Verb1 + 'on the ' + Noun1)
+print('One fell off and ' + Verb2 + 'his head ')
+print('Mama called the ' + Noun2 + 'and the ' + Noun2 + 'said ')
+print('No more ' + Animal + Verb1 + 'on the ' + Noun1+'!')
+
+# 1.Almost 1.5hr
+# 2.Yes, I did
+# 3.I went to the youtube to see if I can find anything to do the project perfectly, but there were a lot of other ways to complete this which I don't know yet.So firstly I got confuse. But sorted it out lastly.
+# 4.There is a lot of way to do the code, but you need to follow in which you are good.
+# 5.I searched for the rhymes,ASCII art,color and bold implementation.
+
+
+
+
